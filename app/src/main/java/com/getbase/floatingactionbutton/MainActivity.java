8b7b98dd82cab73e01918afb98d79194c1bef504
Jerzy Chalupski <jerzy@getbase.com>
2014-09-17 13:19:22 +0200

    Remove hidden views

--- app/src/main/java/com/getbase/floatingactionbutton/MainActivity.java
+++ app/src/main/java/com/getbase/floatingactionbutton/MainActivity.java
@@ -1,15 +1,12 @@
 package com.getbase.floatingactionbutton;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
-    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, Cheeses.sCheeseStrings));
   }
 }
