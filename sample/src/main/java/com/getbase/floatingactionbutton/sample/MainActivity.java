71b66b4827f4c7951ae0655530339c09a4546d14
colymore <rbrlnx@gmail.com>
2015-01-11 20:45:16 +0100

    Support for remove child button on menu

--- sample/src/main/java/com/getbase/floatingactionbutton/sample/MainActivity.java
+++ sample/src/main/java/com/getbase/floatingactionbutton/sample/MainActivity.java
@@ -12,47 +12,54 @@ import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    findViewById(R.id.pink_icon).setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Toast.makeText(MainActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
-      }
-    });
+        findViewById(R.id.pink_icon).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked pink Floating Action Button", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
-    button.setSize(FloatingActionButton.SIZE_MINI);
-    button.setColorNormalResId(R.color.pink);
-    button.setColorPressedResId(R.color.pink_pressed);
-    button.setIcon(R.drawable.ic_fab_star);
-    button.setStrokeVisible(false);
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.setter);
+        button.setSize(FloatingActionButton.SIZE_MINI);
+        button.setColorNormalResId(R.color.pink);
+        button.setColorPressedResId(R.color.pink_pressed);
+        button.setIcon(R.drawable.ic_fab_star);
+        button.setStrokeVisible(false);
 
-    final View actionB = findViewById(R.id.action_b);
+        final View actionB = findViewById(R.id.action_b);
+        final FloatingActionButton actionRemove = (FloatingActionButton) findViewById(R.id.action_remove);
+        actionRemove.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((FloatingActionsMenu) findViewById(R.id.multiple_actions)).removeButton(actionRemove);
+            }
+        });
 
-    FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
-    actionC.setTitle("Hide/Show Action B");
-    actionC.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
-      }
-    });
-    ((FloatingActionsMenu) findViewById(R.id.multiple_actions)).addButton(actionC);
+        FloatingActionButton actionC = new FloatingActionButton(getBaseContext());
+        actionC.setTitle("Hide/Show Action B");
+        actionC.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionB.setVisibility(actionB.getVisibility() == View.GONE ? View.VISIBLE : View.GONE);
+            }
+        });
+        ((FloatingActionsMenu) findViewById(R.id.multiple_actions)).addButton(actionC);
 
-    ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
-    drawable.getPaint().setColor(getResources().getColor(R.color.white));
-    ((FloatingActionButton) findViewById(R.id.setter_drawable)).setIconDrawable(drawable);
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(getResources().getColor(R.color.white));
+        ((FloatingActionButton) findViewById(R.id.setter_drawable)).setIconDrawable(drawable);
 
-    final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
-    actionA.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        actionA.setTitle("Action A clicked");
-      }
-    });
-  }
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
+    }
 }
